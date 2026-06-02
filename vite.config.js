34b9601ff6ca93bc5d245893cfbd5c0029a61552
ilpoppattuso <matteo.fer07+github@gmail.com>
2026-06-02 18:13:19 +0200

    modifiche

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Forza Vite ad usare percorsi assoluti dalla root, fondamentale per i domini personalizzati
+  base: '/', 
+});
